0777a95bc7fac2cdd3f4206c62b48de9e766a53a
Chris Haynes (mac) <chris.haynes@gmail.com>
2014-12-12 14:43:16 +0000

    init

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //(function() {
 
   navigator.serviceWorker.register('/r3search/worker.js', {
-    scope: 'r3search'
+    scope: '/r3search'
   }).then(function(reg) {
     console.log('success', reg);
   }, function(err) {
