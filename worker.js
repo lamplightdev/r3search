d77de6031df37e14fef075ffed497b4f495af645
Chris Haynes (mac) <chris.haynes@gmail.com>
2014-12-13 18:42:55 +0000

    updates

--- worker.js
+++ worker.js
@@ -1,10 +1,13 @@
 importScripts('js/serviceworker-cache-polyfill.js');
+//vbump2
+var cacheVersion = 'test-14';
 
 self.addEventListener('install', function (event) {
+  console.log('install');
   event.waitUntil(
-    caches.open('test-cache')
+    caches.open(cacheVersion)
       .then(function(cache) {
-        console.log('Opened cache');
+        console.log('Opened cache: ' + cacheVersion);
         return cache.addAll([
           'index.html',
           'js/app.js',
@@ -16,6 +19,10 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
+  console.log('fetch: ' + cacheVersion);
+  caches.keys().then(function(cacheNames) {
+    console.log(cacheNames);
+  });
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
@@ -28,5 +35,20 @@ self.addEventListener('fetch', function (event) {
 
         return fetch(event.request);
       })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  console.log('activate');
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== cacheVersion) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
