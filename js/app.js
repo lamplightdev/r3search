01012004ba7cee2d7a1ead0d53cf9993254a5e5a
Chris Haynes (mac) <chris.haynes@gmail.com>
2014-12-16 13:27:45 +0000

    search results

--- js/app.js
+++ js/app.js
@@ -1,37 +1,151 @@
 (function() {
 
-  navigator.serviceWorker.register('/r3search/worker.js', {
-    scope: '/r3search/'
-  }).then(function(reg) {
-    console.log('success', reg);
-  }, function(err) {
-    console.log('fail', err);
-  });
+  var lsQueryName = 'r3search-ls-queries';
+  var queriesContainer, resultsContainer;
+
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/r3search/worker.js', {
+      scope: '/r3search/'
+    }).then(function(reg) {
+      console.log('success', reg);
+    }, function(err) {
+      console.log('fail', err);
+    });
+  }
 
   document.addEventListener("DOMContentLoaded", function (event) {
+    queriesContainer = document.querySelector('.queries');
+    resultsContainer = document.querySelector('.results');
+
+    queriesContainer.addEventListener('click', clickFind);
+    resultsContainer.addEventListener('click', clickFind);
+    listQueries();
+
     var links = document.querySelectorAll('.ajax-get');
-    for(var i=0; i<links.length;i++) {
+    for(var i=0; i<links.length; i++) {
       links[i].addEventListener('click', linkClick);
     }
 
     var form = document.querySelector('.search');
-    form.addEventListener('submit', search);
+    form.addEventListener('submit', formSubmit);
   });
 
-  function search (event) {
+  function clickFind (event) {
+    if (event.target.classList.contains('query-load')) {
+      event.preventDefault();
+
+      var query = event.target.dataset.query;
+      find(query);
+    }
+  }
+
+  function listQueries () {
+    var queries = localStorage.getItem(lsQueryName);
+    if (queries) {
+      queriesContainer.innerHTML = '';
+      queries = JSON.parse(queries);
+      queries.forEach(function (query) {
+        var link = document.createElement('a');
+        link.textContent = query;
+        link.setAttribute('href', '#');
+        link.setAttribute('data-query', query);
+        link.setAttribute('class', 'query-load');
+        queriesContainer.appendChild(link);
+      });
+    }
+  }
+
+  function saveQuery (query) {
+    var queries = localStorage.getItem(lsQueryName);
+    if (queries) {
+      queries = JSON.parse(queries);
+    } else {
+      queries = [];
+    }
+
+    if(queries.indexOf(query) === -1) {
+      queries.push(query);
+    }
+
+    localStorage.setItem(lsQueryName, JSON.stringify(queries));
+
+    listQueries();
+  }
+
+  function formSubmit (event) {
     event.preventDefault();
 
+    search(this.query.value);
+  }
+
+  function search (query) {
+    jsonp('http://en.wikipedia.org/w/api.php', {
+      format: 'json',
+      action: 'opensearch',
+      search: query,
+      limit: 10,
+      titles: query
+    }, function (response) {
+      console.log('success', response);
+
+      resultsContainer.innerHTML = '';
+
+      var results = response[1];
+      results.forEach(function (result) {
+        var link = document.createElement('a');
+        link.textContent = result;
+        link.setAttribute('href', '#');
+        link.setAttribute('data-query', result);
+        link.setAttribute('class', 'query-load');
+        resultsContainer.appendChild(link);
+      });
+
+    }, function (err) {
+      console.log('error', err);
+    });
+  }
+
+  function find (query) {
     jsonp('http://en.wikipedia.org/w/api.php', {
       format: 'json',
       action: 'query',
       continue: '',
       prop: 'extracts|pageimages',
+      exsentences: 10,
+      exlimit: 1,
+      explaintext: true,
+      piprop: 'thumbnail',
+      pithumbsize: 200,
       redirects: true,
-      titles: this.query.value
+      titles: query
     }, function (response) {
       console.log('success', response);
       for(var pageId in response.query.pages) {
-        document.querySelectorAll('.article')[0].innerHTML = response.query.pages[pageId].extract;
+        document.querySelector('.article').innerHTML = '';
+
+        var extract = response.query.pages[pageId].extract;
+        var title = response.query.pages[pageId].title;
+        var thumb = false;
+
+        if (response.query.pages[pageId].thumbnail && response.query.pages[pageId].thumbnail.source) {
+          thumb = response.query.pages[pageId].thumbnail.source;
+        }
+
+        if (!extract) {
+          extract = 'No matching article';
+        } else {
+          saveQuery(title);
+        }
+
+        if (thumb) {
+          var img = document.createElement('img');
+          img.setAttribute('src', thumb);
+          img.setAttribute('class', 'thumb');
+          document.querySelector('.article').appendChild(img);
+        }
+
+        document.querySelector('.article').innerHTML += extract;
+        document.querySelector('.title').textContent = title;
       }
     }, function (err) {
       console.log('error', err);
